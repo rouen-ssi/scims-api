f2c8de4e8b3c82a8c7e9203d17344dbabbeb6eb3
Mathieu Brochard <mathieu.brochard@gmx.fr>
2016-10-31 21:05:25 +0100

    Add article creation endpoint

---
+++ app/SciMS/Controllers/ArticleController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace SciMS\Controllers;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use SciMS\Models\User;
+use SciMS\Models\Article;
+
+class ArticleController {
+
+  public function post(ServerRequestInterface $request, ResponseInterface $response) {
+    $body = $request->getParsedBody();
+
+    // Retreives the User from TokenMiddleware
+    $user = $request->getAttribute('user');
+
+    $article = new Article();
+    $article->setUserId($user->getId());
+    $article->setTitle($body['title']);
+    $article->setContent($body['content']);
+
+    if (!$article->validate()) {
+      $errors = [];
+      foreach ($article->getValidationFailures() as $failure) {
+        $errors[] = $failure->getMessage();
+      }
+
+      return $response->withJson(array(
+        'errors' => $errors
+      ), 400);
+    }
+
+    $article->save();
+
+    return $response->withJson(array(
+      'url' => 'blabla'
+    ), 200);
+  }
+
+}
--- app/index.php
+++ app/index.php
@@ -5,10 +5,13 @@ include '../config/config.php';
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use SciMS\Middlewares\TokenMiddleware;
 
 $app = new \Slim\App();
 
 $app->post('/signup', 'SciMS\Controllers\SignUpController:post');
 $app->post('/signin', 'SciMS\Controllers\SignInController:post');
+$app->post('/article', 'SciMS\Controllers\ArticleController:post')
+  ->add(new TokenMiddleware());
 
 $app->run();
