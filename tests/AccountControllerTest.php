0be3c94b3623aedc9f670cb0441ddc40d0ccc486
Mathieu Brochard <mathieu.brochard@gmx.fr>
2016-11-28 14:45:13 +0100

    Add account creation unit test

---
+++ tests/AccountControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once './vendor/autoload.php';
+require_once './config/config.php';
+require_once 'ControllerTest.php';
+
+use Slim\Http\Response;
+use SciMS\Controllers\AccountController;
+
+class AccountControllerTest extends ControllerTest {
+    public function testSignup() {
+        $request = parent::createRequest('POST', '/account/create');
+        $response = new Response();
+        $accountController = new AccountController();
+        $response = $accountController->create($request, $response)->getBody();
+        $json = json_decode($response);
+
+        $expectedErrors = [
+            'INVALID_EMAIL',
+            'INVALID_FIRST_NAME',
+            'INVALID_LAST_NAME',
+            'INVALID_PASSWORD'
+        ];
+
+        parent::assertEquals($expectedErrors, $json->errors);
+    }
+}
---
+++ tests/ControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once './vendor/autoload.php';
+require_once './config/config.php';
+
+use PHPUnit\Framework\TestCase;
+use Slim\Http\Environment;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class ControllerTest extends TestCase {
+    protected function createRequest($method, $uri) {
+        $env = Environment::mock([
+            'REQUEST_METHOD' => $method,
+            'REQUEST_URI' => $uri
+        ]);
+        
+        return Request::createFromEnvironment($env);
+    }
+}
